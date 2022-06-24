c7d5cdcd39534933a1ead7663e9082a81554dac9
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-06-24 18:52:10 +0300

    24.06.2022

---
+++ magazin_online/magazin/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Client)
+admin.site.register(Produs)
+admin.site.register(Comanda)
+admin.site.register(ComandaProdus)
+admin.site.register(AdresaComanda)
