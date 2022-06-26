39fc2bd07e6993db498013452526247e20a3c7bd
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-06-26 20:04:16 +0300

    26.06.2022

--- ticketSite/ticketApp/urls.py
+++ ticketSite/ticketApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ticket/<int:ticket_id>', views.ticket_by_id, name='ticket_by_id')
+    path('ticket/<int:ticket_id>', views.ticket_by_id, name='ticket_by_id'),
+    path('login_request/', views.login_request, name='login_request'),
+    path('register_request/', views.register_request, name='register_request'),
+    path('logout_request/', views.logout_request, name='logout_request'),
 ]
--- ticketSite/ticketApp/views.py
+++ ticketSite/ticketApp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from django.shortcuts import render
+from .forms import NewAccountForm
 from .models import Ticket
-from django.http import HttpResponse
 
 
 def index(request):
@@ -11,3 +14,41 @@ def index(request):
 def ticket_by_id(request, ticket_id):
 	ticket = Ticket.objects.get(pk=ticket_id)
 	return render(request, 'ticket_by_id.html', {'ticket':ticket})
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewAccountForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			email = form.cleaned_data.get('email')
+			user = authenticate(username=username, password=password, email=email)
+			login(request, user)
+			return render(request, "index.html")
+	form = NewAccountForm()
+	return render(request, "register_request.html", {"form": form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return render(request, "index.html")
+			else:
+				messages.success(request, "Incorrect username or password.")
+		else:
+			messages.success(request, "Incorrect username or password.")
+	form = AuthenticationForm()
+	return render(request, "login_request.html", {'form': form})
+
+
+def logout_request(request):
+	logout(request)
+	return render(request, "index.html")
