6598b89126ddaf2b90d609b82c93a11076b08d3c
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-06-29 22:54:31 +0300

    29.06.2022

--- ticketSite/ticketApp/urls.py
+++ ticketSite/ticketApp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login_request/', views.login_request, name='login_request'),
     path('register_request/', views.register_request, name='register_request'),
     path('logout_request/', views.logout_request, name='logout_request'),
-    path('CreateTicket/', views.CreateTicket, name='CreateTicket'),
+    path('CreateTicket/', views.create_ticket, name='CreateTicket'),
+    path('EditTicket<ticket_id>/', views.edit_ticket, name='EditTicket'),
+    path('DeleteTicket<ticket_id>/', views.delete_ticket, name='DeleteTicket'),
 ]
--- ticketSite/ticketApp/views.py
+++ ticketSite/ticketApp/views.py
@@ -3,17 +3,38 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import NewAccountForm, CreateTicketForm
-from .models import Ticket
+from .models import *
 
 
-def CreateTicket(request):
-	if request.method == "POST":
-		form = CreateTicketForm(request.POST)
+def create_ticket(request):
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			form = CreateTicketForm(request.POST)
+			if form.is_valid():
+				form.save()
+				return redirect('index')
+		form = CreateTicketForm
+		return render(request, 'CreateTicket.html', {'form': form})
+	return render(request, 'index.html')
+
+
+def edit_ticket(request, ticket_id):
+	if request.user.is_authenticated:
+		ticket = Ticket.objects.get(pk=ticket_id)
+		form = CreateTicketForm(request.POST or None, instance=ticket)
 		if form.is_valid():
 			form.save()
 			return redirect('index')
-	form = CreateTicketForm
-	return render(request, 'CreateTicket.html', {'form': form})
+		return render(request, 'EditTicket.html', {'ticket': ticket, 'form': form})
+	return render(request, 'index.html')
+
+
+def delete_ticket(request, ticket_id):
+	if request.user.is_authenticated:
+		ticket = Ticket.objects.get(pk=ticket_id)
+		ticket.delete()
+		return redirect('index')
+	return render(request, 'index.html')
 
 
 def index(request):
@@ -22,8 +43,10 @@ def index(request):
 
 
 def ticket_by_id(request, ticket_id):
-	ticket = Ticket.objects.get(pk=ticket_id)
-	return render(request, 'ticket_by_id.html', {'ticket': ticket})
+	if request.user.is_authenticated:
+		ticket = Ticket.objects.get(pk=ticket_id)
+		return render(request, 'ticket_by_id.html', {'ticket': ticket})
+	return render(request, 'index.html')
 
 
 def register_request(request):
@@ -34,7 +57,11 @@ def register_request(request):
 			username = form.cleaned_data.get('username')
 			password = form.cleaned_data.get('password')
 			email = form.cleaned_data.get('email')
-			user = authenticate(username=username, password=password, email=email)
+			comp = form.cleaned_data.get('compartment')
+			user = form.save()
+			user.set_password(user.password)
+			user.save()
+			user = authenticate(username=username, password=password, email=email, compartment=comp)
 			login(request, user)
 			return redirect('index')
 	form = NewAccountForm()
@@ -51,6 +78,9 @@ def login_request(request):
 			if user is not None:
 				login(request, user)
 				return redirect('index')
+			else:
+				messages.success(request, ("There Was An Error Logging In, Try Again..."))
+				return redirect('login_request')
 	form = AuthenticationForm()
 	return render(request, "login_request.html", {'form': form})
 
