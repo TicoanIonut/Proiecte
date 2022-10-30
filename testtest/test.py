bf5cd92d14b57af452b540a84b45e6f09bbbb795
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-10-30 19:20:57 +0200

    30.10.2022

--- testtest/test.py
+++ testtest/test.py
@@ -1,15 +1,24 @@
 import pandas as pd
 
-one = [1, 2, 3, 4, 5]
-due = [['unu', 'unu', 'unu', 'unu', 'unu'],
-       ['doi', 'doi', 'doi', 'doi', 'doi'],
-       ['trei', 'trei', 'trei', 'trei', 'trei'],
-       ['patru', 'patru', 'patru', 'patru', 'patru'],
-       ['cinci', 'cinci', 'cinci', 'cinci', 'cinci']]
-df = pd.DataFrame(due)
-df = df.transpose()
-df.loc[df.shape[0]] = one
-print(df)
-rs = df.to_json('rez.json')
-df = df.to_csv('rez.csv')
+# one = [1, 2, 3, 4, 5]
+# due = [['unu', 'unu', 'unu', 'unu', 'unu'],
+#        ['doi', 'doi', 'doi', 'doi', 'doi'],
+#        ['trei', 'trei', 'trei', 'trei', 'trei'],
+#        ['patru', 'patru', 'patru', 'patru', 'patru'],
+#        ['cinci', 'cinci', 'cinci', 'cinci', 'cinci']]
+# df = pd.DataFrame(due)
+# df = df.transpose()
+# df.loc[df.shape[0]] = one
+# print(df)
+# rs = df.to_json('rez.json')
+# df = df.to_csv('rez.csv')
 
+
+
+# df = pd.read_csv('datadog.csv')
+# print(df)
+# for data in df.itertuples():
+#        print(data.Breed)
+
+df = pd.read_excel('LISTA-AGENTIILOR-DE-TURISM-LICENTIATE-actualizare-13.10.2022.xlsx')
+print(df)
