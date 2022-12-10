e48db3fbe9ea2537d3c5e8e29d428003e8ed9cc3
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-12-10 23:53:34 +0200

    10.12.2022

--- online_store/ecomm/urls.py
+++ online_store/ecomm/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	path('product/<slug:slug>/', ProductDetailView.as_view(), name='productdetail'),
 	path('addtocart<int:pro_id>/', AddToCartView.as_view(), name='addtocart'),
 	path('mycart/', MyCartView.as_view(), name='mycart'),
-	
+	path('managecart/<int:cp_id>/', ManageCartView.as_view(), name='managecart')
 	
 	
 ]
