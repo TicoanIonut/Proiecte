3e9417debf75dd098cc976fecc0a5d8737c4ada2
Ticoan Ionut <iouticoan@gmail.com>
2023-01-22 17:00:52 +0200

    22.01.2023

--- Calcul/Calcul/settings.py
+++ Calcul/Calcul/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'calculator'
+    'calculator',
+    'chatbotGPT'
 ]
 
 MIDDLEWARE = [
@@ -117,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
