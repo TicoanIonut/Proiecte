fdfc9d8983e5faceae7962f59a92a34bc9c75e2d
Ticoan Ionut <iouticoan@gmail.com>
2023-02-02 21:01:51 +0200

    02.02.2023

--- Calcul/chatbotGPT/urls.py
+++ Calcul/chatbotGPT/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 from .views import *
 
-# urlpatterns = [
-# 	path('chatbot', views.chatbot_response, name='chatbot'),
-#
-# ]
-urlpatterns = [    path('chatbot', ChatbotView.as_view(), name='chatbot'),]
+urlpatterns = [
+	path('chatbot', views.chatbot_response, name='chatbot'),
+	path('answer_question', views.answer_question, name='answer_question'),
+
+]
+# urlpatterns = [    path('chatbot', ChatbotView.as_view(), name='chatbot'),]
--- Calcul/chatbotGPT/views.py
+++ Calcul/chatbotGPT/views.py
@@ -1,20 +1,29 @@
 import openai
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views import View
 from api_secrets import API_KEY
+from .models import ChatbotResponse
 
 openai.api_key = API_KEY
 
+# @login_required
 
-class ChatbotView(View):
-    def get(self, request):
-        return render(request, 'chatbot.html')
 
-    def post(self, request):
+def chatbot_response(request):
+    if request.method == 'POST':
         user_input = request.POST.get('user_input')
         response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_input,
-            max_tokens=1000,
-            temperature=1,)
-        return render(request, 'chatbot.html', {'response': response['choices'][0]['text']})
+            engine="text-davinci-003",prompt=user_input,max_tokens=1000,temperature=1,)
+        response_text = response['choices'][0]['text']
+        # ChatbotResponse.objects.create(question=user_input, answer=response_text)
+        ChatbotResponse.objects.create(user=request.user, question=user_input, answer=response_text)
+        context = {'question': user_input, 'answer': response_text}
+        return render(request, 'chatbot.html', context)
+    return render(request, 'chatbot.html')
+
+
+def answer_question(request):
+    rendering = ChatbotResponse.objects.all()
+    context = {'rendering': rendering}
+    return render(request, 'answer_question.html', context)
