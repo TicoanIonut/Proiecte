cb1fda6fd4c4d882abc708ce49e9f8cee0d257b0
Ticoan Ionut <iouticoan@gmail.com>
2023-02-02 21:14:38 +0200

    02.02.2023

--- Calcul/chatbotGPT/views.py
+++ Calcul/chatbotGPT/views.py
@@ -1,22 +1,17 @@
 import openai
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.views import View
 from api_secrets import API_KEY
 from .models import ChatbotResponse
 
 openai.api_key = API_KEY
-
-# @login_required
 
 
 def chatbot_response(request):
     if request.method == 'POST':
         user_input = request.POST.get('user_input')
         response = openai.Completion.create(
-            engine="text-davinci-003",prompt=user_input,max_tokens=1000,temperature=1,)
+            engine="text-davinci-003", prompt=user_input, max_tokens=1000, temperature=1)
         response_text = response['choices'][0]['text']
-        # ChatbotResponse.objects.create(question=user_input, answer=response_text)
         ChatbotResponse.objects.create(user=request.user, question=user_input, answer=response_text)
         context = {'question': user_input, 'answer': response_text}
         return render(request, 'chatbot.html', context)
