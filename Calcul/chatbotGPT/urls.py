daab10d96398ea7f1df44c25c4903c6ba72339e1
Ticoan Ionut <iouticoan@gmail.com>
2023-02-03 18:24:57 +0200

    03.02.2023

--- Calcul/chatbotGPT/urls.py
+++ Calcul/chatbotGPT/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from . import views
-from .views import *
 
 urlpatterns = [
 	path('chatbot', views.chatbot_response, name='chatbot'),
-	path('answer_question', views.answer_question, name='answer_question'),
-
 ]
-# urlpatterns = [    path('chatbot', ChatbotView.as_view(), name='chatbot'),]
