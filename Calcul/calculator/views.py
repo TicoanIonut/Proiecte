5184a6b6423182e19ea700e70fcbb6ad96b697dd
Ticoan Ionut <iouticoan@gmail.com>
2023-02-09 14:52:39 +0200

    09.02.2023

--- Calcul/calculator/views.py
+++ Calcul/calculator/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def home(request):
+	return render(request, 'index.html')
+
+
 def calc(request):
 	if request.method == 'POST':
 		num1 = request.POST['num1']
@@ -8,28 +12,28 @@ def calc(request):
 		try:
 			if 'add' in request.POST:
 				res = int(num1) + int(num2)
-				return render(request, 'index.html', {'res': res})
+				return render(request, 'calc.html', {'res': res})
 			if 'sub' in request.POST:
 				res = int(num1) - int(num2)
-				return render(request, 'index.html', {'res': res})
+				return render(request, 'calc.html', {'res': res})
 			if 'div' in request.POST:
 				res = int(num1) / int(num2)
-				return render(request, 'index.html', {'res': res})
+				return render(request, 'calc.html', {'res': res})
 			if 'mul' in request.POST:
 				res = int(num1) * int(num2)
-				return render(request, 'index.html', {'res': res})
+				return render(request, 'calc.html', {'res': res})
 			if 'pow' in request.POST:
 				res = int(num1) ** int(num2)
-				return render(request, 'index.html', {'res': res})
+				return render(request, 'calc.html', {'res': res})
 			if 'fdiv' in request.POST:
 				res = int(num1) // int(num2)
-				return render(request, 'index.html', {'res': res})
+				return render(request, 'calc.html', {'res': res})
 			if 'rem' in request.POST:
 				res = int(num1) % int(num2)
-				return render(request, 'index.html', {'res': res})
+				return render(request, 'calc.html', {'res': res})
 		except ZeroDivisionError:
-			return render(request, 'index.html', {"error": "Can't divide by 0"})
+			return render(request, 'calc.html', {"error": "Can't divide by 0"})
 		except ValueError:
-			return render(request, 'index.html', {"error": "You need to type two numbers"})
+			return render(request, 'calc.html', {"error": "You need to type two numbers"})
 			
-	return render(request, 'index.html')
+	return render(request, 'calc.html')
