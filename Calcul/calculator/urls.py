6991dfc261dc90d56406d31d6eee61d625f3a218
Ticoan Ionut <iouticoan@gmail.com>
2023-02-14 12:04:13 +0200

    14.02.2023 Fix

--- Calcul/calculator/urls.py
+++ Calcul/calculator/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
 	path('', views.home, name='home'),
 	path('calc', views.calc, name='calc'),
+	path('delete_res/<int:calcresponse_id>', views.delete_res, name='delete_res'),
 ]
--- Calcul/chatbotGPT/urls.py
+++ Calcul/chatbotGPT/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('chatbot', views.chatbot_response, name='chatbot'),
+	path('delete_resp/<int:chatbotresponse_id>', views.delete_resp, name='delete_resp'),
 ]
