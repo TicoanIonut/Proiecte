d7be6858ed071c09a30cceffd2e3f23ec9edeae8
Ticoan Ionut <iouticoan@gmail.com>
2023-03-01 19:30:19 +0200

    01.03.2023

--- ChatBlog/social/models.py
+++ ChatBlog/social/models.py
@@ -24,11 +24,9 @@ class Meep(models.Model):
 # create userprofile model
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	follows = models.ManyToManyField('self',
-	                                 related_name='followed_by',
-	                                 symmetrical=False,
-	                                 blank=True)
+	follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
 	date_modified = models.DateTimeField(User, auto_now=True)
+	profile_image = models.ImageField(null=True, blank=True, upload_to='images/')
 
 	def __str__(self):
 		return self.user.username
