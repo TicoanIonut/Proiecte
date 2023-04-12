5b56669744aeb89d9046a1dcadb761caf1298dce
Ticoan Ionut <iouticoan@gmail.com>
2023-04-12 09:53:26 +0300

    hotfix

--- Calcul/user_manage/urls.py
+++ Calcul/user_manage/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import CustomerRegistrationView
+
 
 urlpatterns = [
 	path('login_user', views.login_user, name='login_user'),
 	path('register_user', views.register_user, name='register_user'),
-	# path('register_user', CustomerRegistrationView.as_view(), name='register_user'),
+
 	path('logout_user', views.logout_user, name='logout_user'),
 ]
